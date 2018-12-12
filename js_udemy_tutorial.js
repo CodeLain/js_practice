9f291673eade6a6fc2f0dd066b75be1010162a8d
CodeLain <federico.alberti.gurnerio@gmail.com>
2018-12-12 10:59:32 -0300

    html for console and js file for practice

---
+++ js_udemy_tutorial.js
@@ -0,0 +1,14 @@
+const MY_CONSTANT = "constant ]";
+var first_var = "var1";
+let second_var = "var2";
+third_var = "var3";
+
+console.log("1 is a " + typeof (1));
+console.log("1.1 is a" + typeof (1.1));
+console.log('"hola" is a ' + typeof ("hola"));
+console.log("the boolean the is a" + typeof (true));
+console.log("null is an " + typeof (null));
+console.log(MY_CONSTANT);
+console.log(first_var);
+console.log(second_var);
+console.log(third_var);
